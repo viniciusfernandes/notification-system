f2454bdbe0b1daf6f6568e31b62f8990748e0d5c
Vinicius Fernandes <viniciussf@hotmail.com>
2023-05-21 13:09:22 -0300

    Refatoração do index.html

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,15 +1,14 @@
 var stompClient = null;
 
 function setConnected(connected) {
-    $("#connect").prop("disabled", connected);
     $("#disconnect").prop("disabled", !connected);
     if (connected) {
-        $("#conversation").show();
+        $("#advertisements_grid").show();
     }
     else {
-        $("#conversation").hide();
+        $("#advertisements_grid").hide();
     }
-    $("#greetings").html("");
+    $("#advertisements").html("");
 }
 
 function connect() {
@@ -22,7 +21,7 @@ function connect() {
         stompClient.subscribe(queue, function (response) {
             var notification = JSON.parse(response.body);
             var message = notification.userId +' => '+notification.description;
-            showGreeting(message);
+            showAdvertisements(message);
         });
     });
 }
@@ -35,19 +34,19 @@ function disconnect() {
     console.log("Disconnected");
 }
 
-function sendName() {
-    stompClient.send("/app/hello", {}, JSON.stringify({'name': $("#name").val()}));
-}
 
-function showGreeting(message) {
-    $("#greetings").append("<tr><td>" + message + "</td></tr>");
+function showAdvertisements(message) {
+    $("#advertisements").append("<tr><td>" + message + "</td></tr>");
 }
 
 $(function () {
     $("form").on('submit', function (e) {
         e.preventDefault();
     });
-    $( "#connect" ).click(function() { connect(); });
-    $( "#disconnect" ).click(function() { disconnect(); });
-    $( "#send" ).click(function() { sendName(); });
+    $("#disconnect").click(function() { disconnect(); });
+    $("#send").click(function() { sendName(); });
+    $('#userId').blur(function(){
+        disconnect();
+        connect();
+    })
 });
